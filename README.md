844e09338bbce7d9ccf15294c356e40c9bd39499
Ronnie Schaniel <ronnieschaniel@gmail.com>
2021-11-10 08:51:03 +0100

    readme updated and new example added

--- README.md
+++ README.md
@@ -42,3 +42,47 @@ evaluate<Input, Output>(ruleDefinitions, {input1: 'hello', input2: 'world'});
 // { flag1: true, flag2: true }
 ```
 
+### Another example
+
+```
+interface Input {
+    universityDegree: 'NONE' | 'BACHELOR' | 'MASTER';
+    minimumYearsOfExperience: 0 | 2 | 5 | 8 | 10 | 15;
+    hasBlog: boolean;
+    lastYearsPerformance: 1 | 2 | 3 | 4 | 5;
+}
+
+interface Output {
+    minSalary: number;
+    maxSalary: number;
+}
+
+interface Rule {
+    input: Input;
+    output: Output;
+}
+
+const ruleDefinitions: Rule[] = [
+    {
+        input: { universityDegree: 'MASTER', minimumYearsOfExperience: 8, hasBlog: false, lastYearsPerformance: 3 },
+        output: { minSalary: 120_000, maxSalary: 130_000 }
+    },
+    {
+        input: { universityDegree: 'NONE', minimumYearsOfExperience: 10, hasBlog: true, lastYearsPerformance: 4 },
+        output: { minSalary: 125_000, maxSalary: 135_000 }
+    }
+];
+
+
+let candidateA: Input = { universityDegree: 'MASTER', minimumYearsOfExperience: 8, hasBlog: false, lastYearsPerformance: 3 };
+let candidateB: Input = { universityDegree: 'NONE', minimumYearsOfExperience: 10, hasBlog: true, lastYearsPerformance: 4 };
+
+expect(evaluate<Input, Output>(ruleDefinitions, candidateA)).toEqual({
+    minSalary: 120_000,
+    maxSalary: 130_000,
+});
+expect(evaluate<Input, Output>(ruleDefinitions, candidateB)).toEqual({
+    minSalary: 125_000,
+    maxSalary: 135_000,
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,31 @@
 const checkRuleField = <Input, Output>(args: Input, rule: Rule<Input, Output>, field: keyof Input): boolean =>
-  args[field] === rule.input[field] || rule.input[field] === null;
+    args[field] === rule.input[field] || rule.input[field] === null;
 
 export type Rule<I, O> = { input: I; output: O };
 
 export const evaluate = <Input, Output>(
-  ruleDefinitions: Rule<Input, Output>[],
-  args: Input,
+    ruleDefinitions: Rule<Input, Output>[],
+    args: Input,
 ): Partial<Output> | null => {
-  return ruleDefinitions
-    .filter((rule) => Object.keys(rule.input).every((field) => checkRuleField(args, rule, field as keyof Input)))
-    .map((rule) => {
-      const output: any = {};
+    if (!ruleDefinitions || ruleDefinitions.length === 0) {
+        throw Error('No rules defined');
+    }
+    const matches: Output[] = ruleDefinitions
+        .filter((rule) => Object.keys(rule.input).every((field) => checkRuleField(args, rule, field as keyof Input)))
+        .map((rule) => {
+            const output: any = {};
 
-      Object.keys(rule.output).forEach((key) => {
-        output[key] = rule.output[key as keyof Output];
-      });
+            Object.keys(rule.output).forEach((key) => {
+                output[key] = rule.output[key as keyof Output];
+            });
 
-      return output;
-    })[0];
+            return output;
+        });
+
+    if (matches.length === 0) {
+        console.warn(`No rules matched for ${JSON.stringify(args)}.`);
+        return null;
+    } else {
+        return matches[0];
+    }
 };
